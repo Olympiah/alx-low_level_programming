f1733983018c20e121771cda9a892511e840f24b
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-08 09:20:48 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
 
-int main(void)
-
-{
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
-  puts("\"Programming is like building a multilingual puzzle");   
-  return (0);
-
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
