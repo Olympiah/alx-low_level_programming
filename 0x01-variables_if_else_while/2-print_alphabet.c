3d8ff3253e62a173c761588150ab597ae2a835f5
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-09 02:34:37 -0700

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ctype.h>
+
+/**
+ * main- Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+    char lower;
+    
+    for (lower = 'a'; lower <= 'z'; lower++){
+     
+        putchar(tolower(lower));  
+                
+    }; 
+
+    return(0);
+
+}
