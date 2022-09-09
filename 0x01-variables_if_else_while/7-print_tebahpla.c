70394102144cb0b39a3dd0b9a3cc3d722ec05a49
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-09 04:23:09 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main- Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+  char lower;
+
+  for(lower='z'; lower>='a'; lower--){
+  
+     putchar(lower);
+  };
+
+  putchar('\n');
+
+  return(0); 
+
+}
