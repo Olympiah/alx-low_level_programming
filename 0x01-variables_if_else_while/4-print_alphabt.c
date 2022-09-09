6e002c6e914be0fb659196d3e95feb4c382b1c63
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-09 03:30:50 -0700

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main- Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	 char lower;
+
+	 for(lower='a'; lower <= 'z';lower++){
+	 
+	    if(lower != 'q' && lower != 'e'){
+	          putchar(tolower(lower));
+	    };
+		    
+	 };
+
+	 return(0);
+
+}
