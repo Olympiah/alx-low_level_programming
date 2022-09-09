d3ddccb0846e45ab3396bf7139c69ece88f47b24
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-09 02:46:04 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main- Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+   char lower;
+   char upper;
+
+   for(lower = 'a'; lower <= 'z'; lower++){
+     putchar(tolower(lower));
+   };
+
+   for(upper = 'a'; upper <= 'z'; upper++){
+     putchar(toupper(upper));
+   };
+
+   return(0);
+}
