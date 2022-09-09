1f24b5ce8b365536d4bdf888190f1a687d7f14b0
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-09 03:43:05 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+   int x;
+
+   for(x=0; x<=10; x++){
+     printf("%d\n", x);
+   };
+
+   return(0);
+
+}
