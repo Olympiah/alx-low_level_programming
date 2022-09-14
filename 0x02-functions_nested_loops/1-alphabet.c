ee228491aa65aba15ecf41cbfec33a4a5543fe43
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-14 13:01:32 -0700

    alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,18 @@
 #include "main.h"
 
 /**
- * main -Entry Point
+ * print_alphabet -  function that prints the alphabet,
+ * in lowercase, followed by a new line.
  *
- * Return: Always 0 (success)
  */
 
-int main(void)
+void print_alphabet(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-	{ 
-		_putchar(c);	
+	{
+		_putchar(c);
 	};
 _putchar('\n');
-
-return (0);
 }
