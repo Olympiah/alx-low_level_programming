0c4f1cbb30d6542e084f7bfb2bc82616a56c3259
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-14 14:42:03 -0700

    Nature made

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (success)
+ */
+
+
+int main(void)
+{
+	int num, sum = 0;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((n % 3) == 0 && (n % 5) == 0)
+			sum +=num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
