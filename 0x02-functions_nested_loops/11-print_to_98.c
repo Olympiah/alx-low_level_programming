f063a944f5eb97ba96b7b48d5433598f90d9c41e
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-14 14:27:47 -0700

    betty

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_to_98 - function that prints all natural numbers from n to 98, 
+ * print_to_98 - function that prints all natural numbers from n to 98,
  * followed by a new line.
  * @n: the int number that is inputed
  * Return: 0 on (success)
