eaf5c4d745199733ff9d8c80aa885811e4d574e8
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-14 08:30:28 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints the 9 times table, starting with 0.
+ */
+
+void times_table(void)
+{
+	int row, col , product;
+	 for (row = 0; row <= 9; row++)
+	 {
+		 _putchar('0');
+
+		 for (col = 0; col <= 9; col++)
+		 {
+			 _putchar(',');
+			 _putchar('_');
+
+			 product = row * col;
+
+			 if (product <= 9)
+			 {
+				 _putchar(' ');
+			 }
+			 else
+				 _putchar((product / 10) + '0');
+			 
+			 _putchar((product % 10) + '0');
+		 }
+		 _putchar('\n');
+	 }
+}
+
