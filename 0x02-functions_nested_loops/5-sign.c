0821efbeaa8840b252996c80da8068434cd35274
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-14 07:19:39 -0700

    description

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry Point
- *
+ * @n is  the integer parameter
  * Return: Always 0 (success)
  */
 
@@ -10,7 +10,7 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{ 
-	_puchar('+');
+	_putchar('+');
         return (1);
 	}
         else if (n < 0)
