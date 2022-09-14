8a8b17ff60264c730af42ba7ea78d164036b0637
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-14 08:38:46 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result.
+ * @x: first int number to be added.
+ * @y: secondint number to be added.
+ *
+ * Return: the value of addition of x and y.
+ */
+
+int add(int x, int y)
+{
+	result = x + y;
+
+	return (result);
+}
