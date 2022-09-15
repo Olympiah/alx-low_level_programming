1e7a05bd64bf76487a167d7d1221f06510353d04
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-15 14:07:36 -0700

    print more numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that prints the numbers, from 0 to 9,
+ *  followed by a new line.
+ *
+ *  Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n == 2 || n == 4)
+		{
+		continue;
+		}
+		else
+		{
+		_putchar(n + '0');
+		}
+	}
+	_puthchar('\n');
+}
