c457c7709208078b759db1093f2d92d7a3dc1d90
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-15 15:00:09 -0700

    fixed error

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <ctype.h>
 
 /**
  * _isupper - function that checks for uppercase character.
@@ -9,10 +8,7 @@
 
 int _isupper(int c)
 {
-	int x;
-
-	x = isupper(c);
-	if (x)
+	if ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
