845aba31a79ad013c95f51a1427f1c69f059bf02
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-15 16:08:35 -0700

    library

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_most_numbers - function that prints the numbers, from 0 to 9,
  *  followed by a new line.
@@ -19,8 +19,8 @@ void print_most_numbers(void)
 		}
 		else
 		{
-		_putchar(n + '0');
+		putchar(n + '0');
 		}
 	}
-	_puthchar('\n');
+	putchar('\n');
 }
