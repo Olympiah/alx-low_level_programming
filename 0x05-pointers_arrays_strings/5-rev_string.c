2ab2a0b4b916af5e26f8b4bac260d5b440c339a0
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-19 12:55:06 -0700

    fixed error

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,23 +9,20 @@
 
 void rev_string(char *s)
 {
-	int i, j, temp, k;
+	int i;
+	int count = 0;
+	char temp = s[0];
 
-	while (s[i] != '\n')
+	while (s[count] != '\0')
+		count++;
+
+	for (i = 0; i < count; i++)
 	{
-		i++;
+		count--;
+		temp = s[i];
+		s[i] = s[count];
+		s[count] = temp;
 	}
-	k = 0;
-	j = i - 1;
-
-	while (k < j)
-	{
-		temp = s[k];
-		s[k] = s[j];
-		s[j] = temp;
-		i++;
-		j--;
-	};
 }
 
 
