31359d4235e5fe199a3fc5da723395b2340f86b4
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-21 22:17:39 -0700

    tobecontinued

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,14 @@
+#include "mai.h"
+
+/**
+ * cap_string - function that capitalizes all words of a string.
+ *
+ * @str: string to be capitalized
+ * Return: string
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0,j;
+
+
