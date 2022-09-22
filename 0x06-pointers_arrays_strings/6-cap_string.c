44f4f24abd16bcd297e25e41c254d7e1c3243edb
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-22 08:41:22 -0700

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,6 +9,27 @@
 
 char *cap_string(char *str)
 {
-	int i = 0,j;
+	int i = 0, j;
+	char sep[] = "\t\n\,;.!?\"() {}";
+
+	while (*(s + i))
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (sep[j] == *(s + i - 1))
+						*(s + 1) -= 'a' - 'A';
+				}
+			}
+		}
+		i++;
+	}
+	return (s);
+}
 
 
