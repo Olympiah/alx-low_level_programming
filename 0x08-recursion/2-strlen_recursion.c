16ea6f816f13f57fe784e935fb9e9d3117283492
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-28 08:27:12 -0700

    length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function that returns the length of a string.
+ *
+ * @s:string
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int count = 0;
+
+	if (*s != '\0')
+	{
+		count++;
+		count += _strlen_recursion(s + 1);
+	}
+	return (count);
+}
