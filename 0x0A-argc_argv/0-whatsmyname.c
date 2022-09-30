71a2d7a1fb5531bdfea95a1950e415602778a24a
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-30 06:35:14 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program that prints its name
+ *@argc: count of number of arguments.
+ *@argv: pointer to string array of args
+ *Return: 0 on success.
+ */
+
+int main(int argc, int *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
