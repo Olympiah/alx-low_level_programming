82080ee5ff47c744d78c82ff26a2cdfce1503e32
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-30 07:28:48 -0700

    Neither irony nor sarcasm

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: the count of arguments.
+ * @argv: the argument vector
+ * Return: 0 on success
+ */
+
+int main(int argc, int *argv[])
+{
+	int i, mult;
+
+	mult = 1;
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mult *= atoi(argv[i]);
+		}
+		printf("%d\n", mult);
+	}
+	return (0);
+}
+
