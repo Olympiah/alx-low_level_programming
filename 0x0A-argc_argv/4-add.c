bacae4bacf5a14ca4e2f1de863c436d3a9ed0ee0
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-09-30 08:03:45 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * main - entry point
+ *
+ * @argc: the count of arguments
+ * @argv: the argument vector.
+ * Return: 0 on success.
+ */
+
+int main(int argc, int *argv[])
+{
+	int sum = 0, i;
+
+	if (argc < 1)
+	{
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
+
+
