5729ff1c560aac34f7955d4480d67d96f356ff70
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-10-04 05:25:08 -0700

    fixed error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,14 @@ char *str_concat(char *s1, char *s2)
 	char *mem;
 	int i, end = 0, join_i = 0;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		s1 = s2 = "";
+		s1 = "";
+	}
+
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 
 	for (i = 0; s1[i] || s2[i]; i++)
