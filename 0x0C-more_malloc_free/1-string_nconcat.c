bae301964f4a88f386e38c2bd4c133ad846d05b9
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-10-06 08:59:19 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,12 +1,60 @@
 #include "main.h"
 
 /**
- * string_nconcat -  function that concatenates two strings.
- * @s1: string to be concatenated
- * @s2: string to be concatenated
- * size: no of btes of s2 to be concatenated
- * Return: pointer to new memory allocation
+ * string_nconcat -  function that concatenates
+ * two strings.
+ * @s1: string to concatenated.
+ * @s2: string
+ * size: size of s2 to be concatenated.
+ * Return: pointer to new memory allocation.
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int p, q, r;
+	char *mem;
+
+	if (s1 == NULL)
+	{
+		p = 0;
+	}
+	else
+	{
+		for (p = 0; s1[p]; p++)
+			;
+
+	}
+	if (s2 == NULL)
+	{
+		q = 0;
+	}
+	else
+	{
+		for (q = 0; s2[q]; q++)
+		{
+			;
+		}
+	}
+	if (q > n)
+	{
+		q = n;
+	}
+
+	mem = malloc(sizeof(char) * (p + q + 1));
+	if (mem == NULL)
+	{
+		return (NULL);
+	}
+
+	for (r = 0; r < p; r++)
+	{
+		mem[r] = s1[r];
+	}
+	for (r = 0; r < q; r++)
+	{
+		mem[r + 1] = s2[r];
+	}
+	mem[i + q] = '\0';
+
+	return (mem);
+}
