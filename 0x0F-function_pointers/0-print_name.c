c91da4198eb3ba49bc239000903ef26d328cdc12
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-10-13 06:07:10 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - function that prints a name.
+ * @name: name to be printed.
+ * @f: function pointer
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+	{
+		return;
+	}
+
+	f(name);
+}
