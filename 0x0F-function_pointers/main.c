6cb79ecdad9645e14ab90d3e0bb12a4433ae7edb
Olympiah <87874970+Olympiah@users.noreply.github.com>
2022-10-13 11:46:41 -0700

    fixed error

--- 0x0F-function_pointers/main.c
+++ 0x0F-function_pointers/main.c
@@ -10,7 +10,6 @@
 int main(int argc, char *argv[])
 {
 	int arg1, arg2, answer;
-	int (*fn)(int, int);
 	char op;
 
 	if (argc != 4)
@@ -19,26 +18,24 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	op = argv[2][0];
+
+	if (op != '+' && op != '-' && op != '/' && op != '*' && op != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
 	arg1 = atoi(argv[1]);
 	arg2 = atoi(argv[3]);
 
-	fn = get_op_func(argv[2]);
-
-	if (!fn)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	op = *argv[2];
-
-	if ((op == "/" || op == "%") && (argv[2] == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	answer = fn(arg1, arg2);
+	answer = (get_op_func(argv[2]))(arg1, arg2);
 
 	printf("%d\n", answer);
 
